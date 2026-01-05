f357ccf163a817747a35b50b4fabf38df9211fd4
jashwanth ram pedelli <jashwanthrampedelli@jashwanths-MacBook-Air.local>
2026-01-05 18:30:43 +0530

    new one

--- Student_module.py
+++ Student_module.py
@@ -1,40 +1,13 @@
-name = input()
-rollno = int(input())
-print("Student Registered")
-print(f"roll no: {rollno}")
-print(f"name: {name}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def register(name,rollno):
-#     print("Student registered")
-#     print(f"name: {name}")
-#     print(f"roll no: {rollno}")
-# def login(username, password):
-#     if username == "admin" and password == "1234":
-#         print("Login successfully")
-#     else:
-#         print("Invalid credentials")
-# name=input()
-# rollno=int(input())
-# register(name,rollno)
-# login(name,rollno)
+def register(name,rollno):
+    print("Student registered")
+    print(f"name: {name}")
+    print(f"roll no: {rollno}")
+def login(username, password):
+    if username == "admin" and password == "1234":
+        print("Login successfully")
+    else:
+        print("Invalid credentials")
+name=input()
+rollno=int(input())
+register(name,rollno)
+login(name,rollno)
