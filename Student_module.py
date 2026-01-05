a806e48f3e1628affc322f3eb3bb537e3ff62c5e
jashwanth ram pedelli <jashwanthrampedelli@jashwanths-MacBook-Air.local>
2026-01-05 18:42:03 +0530

    new one

--- Student_module.py
+++ Student_module.py
@@ -2,7 +2,12 @@ def register(name,rollno):
     print("Student registered")
     print(f"name: {name}")
     print(f"roll no: {rollno}")
+def login(username, password):
+    if username == "admin" and password == "1234":
+        print("Login successfully")
+    else:
+        print("Invalid credentials")
 name=input()
 rollno=int(input())
 register(name,rollno)
-
+login(name,rollno)
