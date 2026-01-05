d71f709f80fb2c301ddc82a28ec2003844275ad1
jashwanth ram pedelli <jashwanthrampedelli@jashwanths-MacBook-Air.local>
2026-01-05 18:39:01 +0530

    asdasd

--- q1.py
+++ q1.py
@@ -14,3 +14,16 @@ for i in range(0,p.length):
     x=checkpass(p[i])
     if(x):
         print(p[i])
+def register(name,rollno):
+    print("Student registered")
+    print(f"name: {name}")
+    print(f"roll no: {rollno}")
+# def login(username, password):
+#     if username == "admin" and password == "1234":
+#         print("Login successfully")
+#     else:
+#         print("Invalid credentials")
+name=input()
+rollno=int(input())
+register(name,rollno)
+# login(name,rollno)
